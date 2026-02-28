441a2dc17adceef4dba2d6226e7bd72fe85cc952
mathebe <mathebemoseri@gmail.com>
2026-02-28 10:06:43 +0200

    feat: add basic POST endpoint for testing

--- StringSorterApi/Controllers/StringSorterController.cs
+++ StringSorterApi/Controllers/StringSorterController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using StringSorterApi.Models;
 
 namespace StringSorterApi.Controllers
 {
@@ -7,5 +8,10 @@ namespace StringSorterApi.Controllers
     [ApiController]
     public class StringSorterController : ControllerBase
     {
+        [HttpPost]
+        public IActionResult Sort([FromBody] SortRequest request)
+        {
+            return Ok("Endpoint working?");
+        }
     }
 }
