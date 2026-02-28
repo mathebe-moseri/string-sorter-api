6a2d3eb2863dca9908e9df9e8413d5c35a72a25e
mathebe <mathebemoseri@gmail.com>
2026-02-28 17:41:41 +0200

    enable CORS to allow Angular frontend requests

--- StringSorterApi/Program.cs
+++ StringSorterApi/Program.cs
@@ -1,19 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngular", policy =>
+        policy.WithOrigins("http://localhost:4200")
+              .AllowAnyHeader()
+              .AllowAnyMethod());
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+
+// Use Cors (must be before MapControllers)
+app.UseCors("AllowAngular");
 
 app.UseAuthorization();
 
